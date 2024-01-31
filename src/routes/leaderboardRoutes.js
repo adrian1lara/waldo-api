6123cd89284a8401f3088e129e896aeac4ee4ed1
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-31 00:47:10 -0500

    added route to delete one leaderboard by id

--- src/routes/leaderboardRoutes.js
+++ src/routes/leaderboardRoutes.js
@@ -3,14 +3,13 @@ const router = express.Router()
 
 const leaderboardController = require("../controllers/leaderboardController")
 
-
-
 // create form of leaderboard
 router.post("/api/leaderboard/submit",  leaderboardController.createLeaderboard)
 
 //get all leaderboard
 router.get("/api/leaderboard", leaderboardController.getLeaderboard)
 
-
+// delete one by id
+router.post("/api/delete/:leaderId", leaderboardController.deleteLeaderById)
 
 module.exports = router
