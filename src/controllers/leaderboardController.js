be10cf720235c8d55f6bd65a5efd5676bcec4b93
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-31 00:48:30 -0500

    line 51 update if, and line 3, create delete leaderbaord by id function

--- src/controllers/leaderboardController.js
+++ src/controllers/leaderboardController.js
@@ -1,13 +1,35 @@
 const Leaderboard = require("../models/leaderboard")
 
+// delete one leaderboard by id
+exports.deleteLeaderById = async(req, res) => {
+    const leaderId = req.params.leaderId
 
+    if(!leaderId) {
+        res.status(400).json({message: "Leader ID is required in the request parameters."})
+    }
+
+    try {
+        const deletedLeader = await Leaderboard.findByIdAndDelete(leaderId)
+
+        if(deletedLeader){ 
+            return res.status(201).json({message: `${deletedLeader} succesfully delete`})
+        } else {
+            return res.status(404).json({ message: "leader not found"})
+        }
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: error.message})
+    }
+   
+}
 
 // get the leaderboard
 exports.getLeaderboard = async (req, res) => {
     try {
         const leaderboards = await Leaderboard.find()
 
-        if(leaderboards.length === 0) {
+        if(leaderboards.length == 0) {
             res.status(404).json({message: "Data not found"})
             return
         }
@@ -26,7 +48,7 @@ exports.createLeaderboard = async (req, res) => {
         
         const { name, timer, clicks } = req.body
 
-        if(name.length === 0) { 
+        if(!name || name.length <= 0) { 
             res.status(400).json({message: "Please enter a name"})
             return
         }
