2e0971207dc42db12300a54213641ac3ba983584
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-30 20:28:01 -0500

    added dotenv, db connection, cors and routing to the api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
+require('dotenv').config()
+require('./config/connectionDB').connect()
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
+const leaderboardRouter = require("./routes/leaderboardRoutes")
 
 app.use(bodyParser.json())
+app.use(cors())
+
+app.use("/", leaderboardRouter)
+
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
